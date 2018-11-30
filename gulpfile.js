120f68d037f4490d9bbd6823f6db01b77d344360
Bertram <jabertra@amazon.com>
2018-11-29 17:35:45 -0800

    set up CSS architecture using BEM model, began to style blocks using gulp/postCSS

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,9 @@ var gulp = require('gulp'),
 watch = require('gulp-watch'), //trigger
 postcss = require('gulp-postcss'), //call postcss pkg
 autoprefixer = require('autoprefixer'), //call autoprefixer for -moz etc
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'); 
+cssvars = require('postcss-simple-vars'), //allows variables
+nested = require('postcss-nested'), //allows nested css
+cssImport = require('postcss-import'); //imports our modules to one css file for browser
 
 gulp.task('default', function(){ console.log("Gulp task created"); });
 gulp.task('html', function(){ console.log("Imagine something useful being done to your HTML here."); });
@@ -13,8 +14,8 @@ gulp.task('styles', function(){
 	//another pipe to apply post css filters, first install with NPM, npm install gulp-postcss --save-dev
 	//add autoprefixer in postcss params using NPM, add css vars from NPM 
 	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([cssvars, nested, autoprefixer]))
-	.pipe(gulp.dest('./app/temp/styles'));
+	.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+	.pipe(gulp.dest('./app/temp/styles')); 
 });
 
 gulp.task('watch', function(){
